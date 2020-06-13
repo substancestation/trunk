7bc26478adb9716c119c274cabdd0e5bf2cbe3fe
Yang Yang <yangyang4cs@gmail.com>
2020-06-13 00:45:16 -0700

    test

--- dev/org.orbit.substance.io/src/org/orbit/substance/io/impl/DFileImpl.java
+++ dev/org.orbit.substance.io/src/org/orbit/substance/io/impl/DFileImpl.java
@@ -406,6 +406,31 @@ public class DFileImpl implements DFile {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((path == null) ? 0 : path.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DFileImpl other = (DFileImpl) obj;
+		if (path == null) {
+			if (other.path != null)
+				return false;
+		} else if (!path.equals(other.path))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "DFileImpl [metadata=" + this.metadata + ", path=" + this.path + "]";
 	}
